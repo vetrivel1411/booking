2c422edd4e3b4fcedf470707d651758ac5106063
vetrivel1411 <vetrivelifet22@gmail.com>
2025-06-26 16:56:53 +0530

    Add files via upload

---
+++ booking-service/src/main/java/com/example/booking/BookingController.java
@@ -0,0 +1,23 @@
+
+package com.example.booking;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/book")
+public class BookingController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @PostMapping
+    public String createBooking(@RequestBody Map<String, String> booking) {
+        String kongBaseUrl = "http://kong:8000/api";
+
+        restTemplate.postForObject(kongBaseUrl + "/payment/pay", booking, String.class);
+        restTemplate.postForObject(kongBaseUrl + "/notify/send", booking, String.class);
+
+        return "Booking created and services triggered.";
+    }
+}
